fcdc9abdfe54a638f52eea3915e95f172d383ffb
WireHallMedic <michaelwidler@yahoo.com>
2022-04-18 18:41:50 -0700

    Add verse 5 to phrase_list

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,8 @@ class House
             "the house that Jack built",
             "the malt that lay in",
             "the rat that ate",
-            "the cat that killed"
+            "the cat that killed",
+            "the dog that worried"
         ]
 
     def line(number, pirate = false)
