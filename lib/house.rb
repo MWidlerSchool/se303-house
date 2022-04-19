6588f23439d1c9d7d438a4741517d8fb2ee0a433
WireHallMedic <michaelwidler@yahoo.com>
2022-04-18 18:15:00 -0700

    Copy house2 over house

--- lib/house.rb
+++ lib/house.rb
@@ -1,33 +1,37 @@
 class House
 
     def line(number)
+        "This is #{inner_text(number)}.\n"
+    end
+
+    def inner_text(number)
         case number
         when 1
-            "This is the house that Jack built.\n"
+            "the house that Jack built"
         when 2
-            "This is the malt that lay in the house that Jack built.\n"
+            "the malt that lay in #{inner_text(number - 1)}"
         when 3
-            "This is the rat that ate the malt that lay in the house that Jack built.\n"
+            "the rat that ate #{inner_text(number - 1)}"
         when 4
-            "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "the cat that killed #{inner_text(number - 1)}"
         when 5
-            "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "the dog that worried #{inner_text(number - 1)}"
         when 6
-            "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "the cow with the crumpled horn that tossed #{inner_text(number - 1)}"
         when 7
-            "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "the maiden all forlorn that milked #{inner_text(number - 1)}"
         when 8
-            "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "the man all tattered and torn that kissed #{inner_text(number - 1)}"
         when 9
-            "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "the priest all shaven and shorn that married #{inner_text(number - 1)}"
         when 10
-            "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "the rooster that crowed in the morn that woke #{inner_text(number - 1)}"
         when 11
-            "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "the farmer sowing his corn that kept #{inner_text(number - 1)}"
         when 12
-            "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "the horse and the hound and the horn that belonged to #{inner_text(number - 1)}"
         else
-            "No line found for #{number}"
+            "no line found for #{number}"
         end
     end
 
