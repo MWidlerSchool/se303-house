71cb7d8f957c3a798ca510ef688d070ee3aac49a
WireHallMedic <michaelwidler@yahoo.com>
2022-04-21 16:11:30 -0700

    Remove case 8, remove duplication

--- lib/house.rb
+++ lib/house.rb
@@ -16,8 +16,6 @@ class House
             "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 7
             "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        when 8
-            "This is the #{line_difference(number)}man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
             "This is the #{line_difference(number)}man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
@@ -33,8 +31,6 @@ class House
             "rooster that crowed in the morn that woke the priest all shaven and shorn that married the "
         when 9
             "priest all shaven and shorn that married the "
-        when 8
-            ""
         else
             ""
         end
