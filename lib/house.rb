eef1823c3cb4bc706fa68c93565b374e03666536
WireHallMedic <michaelwidler@yahoo.com>
2022-04-21 16:23:58 -0700

    Remove duplication

--- lib/house.rb
+++ lib/house.rb
@@ -6,8 +6,6 @@ class House
             "This is the house that Jack built.\n"
         when 2
             "This is the malt that lay in the house that Jack built.\n"
-        when 3
-            "This is the #{line_difference(number)}rat that ate the malt that lay in the house that Jack built.\n"
         else
             "This is the #{line_difference(number)}rat that ate the malt that lay in the house that Jack built.\n"
         end
