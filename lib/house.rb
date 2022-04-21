7d73890cfab041ed5a53c902a31b7ee70eaf1aac
WireHallMedic <michaelwidler@yahoo.com>
2022-04-21 16:18:58 -0700

    Move line 6 chunk to cases 6-12

--- lib/house.rb
+++ lib/house.rb
@@ -13,26 +13,26 @@ class House
         when 5
             "This is the #{line_difference(number)}dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
-            "This is the #{line_difference(number)}cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is the #{line_difference(number)}dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 
     def line_difference(number)
         case number
         when 12
-            "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the "
+            "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the "
         when 11
-            "farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the "
+            "farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the "
         when 10
-            "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the "
+            "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the "
         when 9
-            "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the "
+            "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the "
         when 8
-            "man all tattered and torn that kissed the maiden all forlorn that milked the "
+            "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the "
         when 7
-            "maiden all forlorn that milked the "
+            "maiden all forlorn that milked the cow with the crumpled horn that tossed the "
         when 6
-            ""
+            "cow with the crumpled horn that tossed the "
         else
             ""
         end
