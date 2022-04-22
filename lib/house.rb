ed8264609756f8828e29a5e42975ac7f68578f19
WireHallMedic <michaelwidler@yahoo.com>
2022-04-21 17:41:58 -0700

    Remove duplication

--- lib/house.rb
+++ lib/house.rb
@@ -28,8 +28,6 @@ class House
             "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
         when 9
             "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
-        when 8
-            "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
         end
     end
 
