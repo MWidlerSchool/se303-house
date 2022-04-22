cd70c08bfbfccc580c3925fc21144f30bcff642a
WireHallMedic <michaelwidler@yahoo.com>
2022-04-21 18:08:36 -0700

    Remove unneeded checking for pirates

--- lib/house.rb
+++ lib/house.rb
@@ -16,12 +16,8 @@ class House
         "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
     ]
 
-    def line(number, pirate = false)
-        if pirate
-            pirate_line(number)
-        else
-            landlubber_line(number)
-        end
+    def line(number)
+        "This is the #{@@segment_array[number]}house that Jack built.\n"
     end
 
     def landlubber_line(number)
