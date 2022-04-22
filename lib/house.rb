cf90154b2deb6dfd8867b4f7d2f0ea7fac471fc3
WireHallMedic <michaelwidler@yahoo.com>
2022-04-21 17:40:36 -0700

    Remove duplication

--- lib/house.rb
+++ lib/house.rb
@@ -30,8 +30,6 @@ class House
             "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
         when 7
             "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
-        when 6
-            "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
         end
     end
 
