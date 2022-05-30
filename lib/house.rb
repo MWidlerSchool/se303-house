c3ecdb8e8b2362dbde7f94a055bfa01712285574
WireHallMedic <michaelwidler@yahoo.com>
2022-05-30 11:23:20 -0700

    refactor class variable into object variable

--- lib/house.rb
+++ lib/house.rb
@@ -1,23 +1,25 @@
 class House
 
-    @@segment_array = [
-        "If you can read this, something has gone wrong",
-        "",
-        "malt that lay in the ",
-        "rat that ate the malt that lay in the ",
-        "cat that killed the rat that ate the malt that lay in the ",
-        "dog that worried the cat that killed the rat that ate the malt that lay in the ",
-        "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-        "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-        "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-        "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-        "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-        "farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-        "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
-    ]
+    def initialize()
+        @segment_array = [
+            "If you can read this, something has gone wrong",
+            "",
+            "malt that lay in the ",
+            "rat that ate the malt that lay in the ",
+            "cat that killed the rat that ate the malt that lay in the ",
+            "dog that worried the cat that killed the rat that ate the malt that lay in the ",
+            "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+            "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+            "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+            "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+            "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+            "farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+            "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the ",
+        ]
+    end
 
     def line(number)
-        "This is the #{@@segment_array[number]}house that Jack built.\n"
+        "This is the #{@segment_array[number]}house that Jack built.\n"
     end
 
     def recite()
@@ -27,6 +29,6 @@ end
 
 class PirateHouse < House
     def line(number)
-        "Thar be the #{@@segment_array[number]}house that Jack built.\n"
+        "Thar be the #{@segment_array[number]}house that Jack built.\n"
     end
 end
