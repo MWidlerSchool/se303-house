a13115df488e4c928ed611e107d86d0d05f04031
WireHallMedic <michaelwidler@yahoo.com>
2022-05-30 11:30:30 -0700

    exercise new function

--- lib/house.rb
+++ lib/house.rb
@@ -34,6 +34,7 @@ class House
     end
 
     def line(number)
+        "This is the #{assemble_line(number)}house that Jack built.\n"
         "This is the #{@segment_array[number]}house that Jack built.\n"
     end
 
